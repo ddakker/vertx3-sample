828089c46edfe996fc4e3b3cb0f912946cf6148e
ddakker <ddakker@gmail.com>
2016-03-09 23:18:25 +0900

    .

--- src/main/java/io/vertx/example/core/net/echo/ServerWorker.java
+++ src/main/java/io/vertx/example/core/net/echo/ServerWorker.java
@@ -10,44 +10,34 @@ import io.vertx.example.util.Runner;
 /*
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class Server extends AbstractVerticle {
+public class ServerWorker extends AbstractVerticle {
 
 	// Convenience method so you can run it in your IDE
 	public static void main(String[] args) {
-		Runner.runExample(Server.class);
+		Runner.runExample(ServerWorker.class);
 	}
 
 	@Override
-    public void start() throws Exception {
+	public void start() throws Exception {
+		vertx.deployVerticle("io.vertx.example.core.verticle.worker.WorkerVerticle",
+				new DeploymentOptions().setWorker(true).setInstances(2));
+
 		vertx.createNetServer().connectHandler(socket -> {
 
 			String handlerID = socket.writeHandlerID();
 
-			
-			
 			socket.handler(RecordParser.newDelimited("\n", buffer -> {
-			//socket.handler(buffer -> {
+				// socket.handler(buffer -> {
 
 				String message = buffer.toString().trim();
 
-				//System.out.println(socket.remoteAddress() + " (" + handlerID + ") => " + message + "\r\n");
-				try {
-					Thread.sleep(5000);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				socket.write("Receive => " + message + "\n");
-				if (message.contains("start")) {
-					System.out.println("s: " + new Date());
-				}
-				if (message.contains("end")) {
-					System.out.println("e: " + new Date());
-				}
+				vertx.eventBus().send("sample.data", message, r -> {
+					System.out.println("[Main] Receiving reply ' " + r.result().body() + "' in " + Thread.currentThread().getName());
+					socket.write("Receive => " + message + "\n");
+				});
 
 			}));
 
 		}).listen(1234);
-    }
+	}
 }
